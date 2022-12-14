3ab77a994da103d116e2737e4d87ead222bcafdf
vrbdas <vrbdas@gmail.com>
2022-12-14 16:57:17 +0600

    gulp success

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const gulp        = require('gulp');
 const browserSync = require('browser-sync');
-const sass        = require('gulp-sass');
+const sass        = require('gulp-sass')(require('sass'));
 
 // Static server
 gulp.task('server', function() {
