79a081807f2e68bc2c1582d08759cf2a5b57cc31
vrbdas <vrbdas@gmail.com>
2022-12-15 16:37:23 +0600

    gulp final

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
-const gulp        = require('gulp');
-const browserSync = require('browser-sync');
-const sass        = require('gulp-sass')(require('sass'));
+const gulp          = require('gulp');
+const browserSync   = require('browser-sync');
+const sass          = require('gulp-sass')(require('sass'));
+const rename        = require("gulp-rename");  
+const autoprefixer  = require('gulp-autoprefixer');
+const cleanCSS      = require('gulp-clean-css');
 
 // Static server
 gulp.task('server', function() {
@@ -14,6 +17,15 @@ gulp.task('server', function() {
 gulp.task('styles', function() {
     return gulp.src("src/sass/*.+(scss|sass)")
             .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+            .pipe(rename({
+                prefix: "",
+                suffix: ".min"
+            }))
+            .pipe(autoprefixer({
+                overrideBrowserslist: ['last 2 versions'],
+                cascade: false
+            }))
+            .pipe(cleanCSS({compatibility: 'ie8'}))
             .pipe(gulp.dest("src/css"))
             .pipe(browserSync.stream());
 });
